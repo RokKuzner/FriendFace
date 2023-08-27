83a0012ce5f490c607fbafc3352e77424dc26e7f
Rok <rok.ankaran@gmail.com>
2023-08-27 18:17:51 +0200

    added decorators.py

---
+++ decorators.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+def login_required(f):
+    def decorated_function(*args, **kwargs):
+        if "curren_user" in request.session:
+            if request.session["curren_user"] != None:
+                pass
+            else:
+                return HttpResponseRedirect(reverse('login'))
+        else:
+            return HttpResponseRedirect(reverse('login'))
+        return f(*args, **kwargs)
+
+    return decorated_function
