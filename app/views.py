c2b00a7a540eaa703b54aebfc5bfba31ecaa7cd9
Rok <rok.ankaran@gmail.com>
2023-08-28 12:55:42 +0200

    made register functional

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.shortcuts import render, HttpResponseRedirect, redirect
 from django.urls import reverse
 from decorators import login_required
+import database as db
 
 # Create your views here.
 @login_required
@@ -26,8 +27,20 @@ def login(request):
 def register(request):
     if request.method == 'POST':
         username = request.POST['user']
-        password = request.POST['password']
-        password = request.POST['password2']
-        messages.error(request, "Error")
-        return redirect('register')
+        password1 = request.POST['password']
+        password2 = request.POST['password2']
+
+        if db.user_exists(username):
+            messages.error(request, "User with this username allready exists")
+            return redirect('register')
+        elif len(password1) < 6:
+            messages.error(request, "Password must be at least 6 characters long")
+            return redirect('register')
+        elif password1 != password2:
+            messages.error(request, "Passwords do not match")
+            return redirect('register')
+        else:
+            db.add_user(username, password1)
+            request.session["curren_user"] = username
+            return redirect('home')
     return render(request, 'register.html')
