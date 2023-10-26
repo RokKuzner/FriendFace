49158242a67d8ab4ff834bff5c6599c838ba52b9
Rok <rok.ankaran@gmail.com>
2023-10-26 19:39:31 +0200

    deleted some nonsense from the code caused by the bug that was fixed recently

--- AI_models/genre_prediction/create_and_modify_data.py
+++ AI_models/genre_prediction/create_and_modify_data.py
@@ -45,7 +45,7 @@ for i in json_data:
         if char.isalpha() == True or char == " ":
             description_modifyed += char
     description = description_modifyed
-    if len(description) == 0 or description == str(" "*len(description)) or description.isnumeric() or "the hackers gained more access than the company previously revealed though the attackers were unable to modify code or access emails" in description:
+    if len(description) == 0 or description == str(" "*len(description)) or description.isnumeric():
         pass
     else:
         if i["category"].lower() in list(categoryes_replace.keys()):
