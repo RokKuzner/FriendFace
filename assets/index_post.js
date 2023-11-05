819c9c8906296b0a7bafe9e1e6e3fed2cf9e0987
Rok <rok.ankaran@gmail.com>
2023-11-05 19:05:34 +0100

    collectes static

--- assets/index_post.js
+++ assets/index_post.js
@@ -1,20 +1,14 @@
-console.log(current_user)
+console.info("Current user: "+current_user)
+
 let post_btn = document.getElementById("postbtn")
 let post_text = document.getElementById("posttext")
-let like_btn = document.getElementById("like")
 
 function post_click() {
-    let parts_of_url = String(window.location.href).split("/")
-    let redirect_url = parts_of_url[0] + "post?user="+current_user+"&content="+post_text.value
+    let redirect_url = window.location.origin + "/" + "post?user="+current_user+"&content="+post_text.value
     
     if ( (!(post_text.value == '')) && (!(post_text.value == (' '*post_text.value.length))) ) {
         window.location.replace(redirect_url);
     }
 }
 
-function like_click() {
-    console.log('like!!')
-}
-
 post_btn.addEventListener("click", post_click)
-like_btn.addEventListener("click", like_click)
---
+++ assets/share_post.js
@@ -0,0 +1,16 @@
+async function copy_to_clipboard(text) {
+    try {
+        await navigator.clipboard.writeText(text);
+        return "succes"
+    } catch (err) {
+        return err
+    }
+}
+
+for (let post of document.querySelectorAll(".post")) {
+    let share_div = post.querySelector(".commentoptions .sharediv")
+    share_div.addEventListener("click", e => {
+        let url = window.location.origin+"/getpost/"+post.id
+        copy_to_clipboard(url)
+    })
+}
