b09c97af738e42238c7e90d875d63f0fd060183e
Rok <rok.ankaran@gmail.com>
2023-11-05 18:35:13 +0100

    bugfix changed window.loaction.href with window.loaction.origin

--- static/index_post.js
+++ static/index_post.js
@@ -4,8 +4,7 @@ let post_btn = document.getElementById("postbtn")
 let post_text = document.getElementById("posttext")
 
 function post_click() {
-    let parts_of_url = String(window.location.href).split("/")
-    let redirect_url = parts_of_url[0] + "post?user="+current_user+"&content="+post_text.value
+    let redirect_url = window.location.origin + "/" + "post?user="+current_user+"&content="+post_text.value
     
     if ( (!(post_text.value == '')) && (!(post_text.value == (' '*post_text.value.length))) ) {
         window.location.replace(redirect_url);
--- static/share_post.js
+++ static/share_post.js
@@ -10,7 +10,7 @@ async function copy_to_clipboard(text) {
 for (let post of document.querySelectorAll(".post")) {
     let share_div = post.querySelector(".commentoptions .sharediv")
     share_div.addEventListener("click", e => {
-        let url = String(window.location.href)+"getpost/"+post.id
+        let url = window.location.origin+"/getpost/"+post.id
         copy_to_clipboard(url)
     })
 }
