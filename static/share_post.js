76aef11ec337d6becc36daf74bb1f04c9d38cfbc
Rok <rok.ankaran@gmail.com>
2023-11-05 18:08:28 +0100

    javascript file for copying post url to clipboard

---
+++ static/share_post.js
@@ -0,0 +1,18 @@
+async function copy_to_clipboard(text) {
+    try {
+        await navigator.clipboard.writeText(text);
+        return "succes"
+    } catch (err) {
+        return err
+    }
+}
+
+for (let post of document.querySelectorAll(".post")) {
+    let share_div = post.querySelector(".commentoptions .sharediv")
+    console.log("IN HERE")
+    console.log(share_div)
+    share_div.addEventListener("click", e => {
+        let url = String(window.location.href)+"getpost/"+post.id
+        copy_to_clipboard(url)
+    })
+}
