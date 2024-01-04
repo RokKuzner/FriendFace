8286ab2d7a77255a2e70c7d092c4c267286cba2c
Rok <rok.ankaran@gmail.com>
2024-01-04 17:31:15 +0100

    prevented double posting

--- static/javascript/index_post.js
+++ static/javascript/index_post.js
@@ -1,12 +1,35 @@
 let post_btn = document.getElementById("postbtn")
 let post_text = document.getElementById("posttext")
 
-function post_click() {
-    let redirect_url = window.location.origin + "/" + "post?user="+current_user+"&content="+post_text.value
+async function post_click() {
+    let url = window.location.origin + "/api/post"
+
+    if (post_btn.classList.contains("disabled")) {
+        return null
+    }
     
     if ( (!(post_text.value == '')) && (!(post_text.value == (' '*post_text.value.length))) ) {
-        window.location.replace(redirect_url);
+        post_btn.classList.add("disabled")
+        post_btn.innerHTML = '<img style="width: 30px;" src="/files/static/icons?file=loading.gif">'
+
+        let data = new URLSearchParams();
+        data.append("post_content", post_text.value)
+
+        let response = await fetch(url, {
+            method: "POST",
+            body: data
+        })
+        let json_res = await response.json()
+
+        if (json_res["status"] == "succes") {
+            window.location.reload()
+        }
+
+        post_btn.innerHTML = 'Post'
+        post_btn.classList.remove("disabled")
     }
 }
 
-post_btn.addEventListener("click", post_click)
+post_btn.addEventListener("click", async () => {
+    await post_click()
+})
