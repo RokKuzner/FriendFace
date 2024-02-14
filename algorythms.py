75c712a0f113d789c7c26817d8d7a2bc54783777
Rok <rok.ankaran@gmail.com>
2024-02-14 19:22:34 +0100

    corrected post info index to key

--- algorythms.py
+++ algorythms.py
@@ -32,7 +32,7 @@ def get_personalized_posts(user:str):
         posts_with_interest = db.get_latest_posts_by_genre(user, 200, interest)
 
         for post in posts_with_interest:
-            if (post[0] not in users_following) and (float(post[5]) < float(first_latest_post[5])):
+            if (post["author_username"] not in users_following) and (float(post["time"]) < float(first_latest_post["time"])):
                 posts_to_grade.append(post)
 
     #Create a dict for graded posts
