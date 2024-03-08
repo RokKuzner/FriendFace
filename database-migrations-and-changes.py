a800e8415343102981fbee88fe37231a6c6a9a07
Rok <rok.ankaran@gmail.com>
2024-03-08 13:40:37 +0100

    add keywords to posts without them

--- database-migrations-and-changes.py
+++ database-migrations-and-changes.py
@@ -1,4 +1,5 @@
 import sqlite3
+import keywordextractor as keyex
 
 conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
@@ -7,6 +8,15 @@ CREATE_KEYWORDS_COLUMN = """
 ALTER TABLE posts ADD keywords TEXT;
 """
 c.execute(CREATE_KEYWORDS_COLUMN)
+
+c.execute('SELECT * FROM posts')
+posts = c.fetchall()
+
+for post in posts:
+    if post[6] == None:
+        post_keywords = keyex.extract_keywords(post[1])
+        c.execute('UPDATE posts SET keywords=? WHERE id=?', (",".join(post_keywords), post[4]))
+        conn.commit()
 
 conn.commit()
 conn.close()
