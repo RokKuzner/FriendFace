5d5882061a8286b96e95c198b9caabb0baa91c6c
Rok <rok.ankaran@gmail.com>
2024-03-08 13:14:51 +0100

    calculate number of keywords to extract depending on the length of text

--- keywordextractor.py
+++ keywordextractor.py
@@ -1,10 +1,16 @@
 import yake
 
-extractor = yake.KeywordExtractor(top=10, stopwords=None)
-
 def extract_keywords(text:str):
+    #Get the number of keywords to extract
+    keywords_to_extract = int(len(text.split())*0.15)
+    keywords_to_extract = 3 if keywords_to_extract < 3 else keywords_to_extract
+    keywords_to_extract = 10 if keywords_to_extract > 10 else keywords_to_extract
+    
+    #Extract the keywords
+    extractor = yake.KeywordExtractor(top=keywords_to_extract, stopwords=None)
     keywords = extractor.extract_keywords(text)
 
+    #Return the keywords in an array
     out = []
     for keyword, score in keywords:
         out.append(keyword)
