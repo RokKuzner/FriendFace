4fd893f2f7266f4064f3378932f06c2f9c38dc23
Rok <rok.ankaran@gmail.com>
2024-03-09 19:29:56 +0100

    create keywords table and put in keywords from all posts

--- database-migrations-and-changes.py
+++ database-migrations-and-changes.py
@@ -1,22 +1,31 @@
 import sqlite3
-import keywordextractor as keyex
 
 conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
 
-CREATE_KEYWORDS_COLUMN = """
-ALTER TABLE posts ADD keywords TEXT;
+CREATE_KEYWORDS_TABLE = """
+CREATE TABLE IF NOT EXISTS keywords(
+keyword TEXT UNIQUE,
+posts TEXT
+)
 """
-c.execute(CREATE_KEYWORDS_COLUMN)
+c.execute(CREATE_KEYWORDS_TABLE)
 
 c.execute('SELECT * FROM posts')
 posts = c.fetchall()
 
+keywords_dict = {}
+
 for post in posts:
-    if post[6] == None:
-        post_keywords = keyex.extract_keywords(post[1])
-        c.execute('UPDATE posts SET keywords=? WHERE id=?', (",".join(post_keywords), post[4]))
-        conn.commit()
+    post_keywords = post[6].split(",")
+    for keyword in post_keywords:
+        try:
+            keywords_dict[keyword].append(post[4])
+        except KeyError:
+            keywords_dict[keyword] = [post[4]]
+
+for keyword, posts in keywords_dict.items():
+    c.execute('INSERT INTO keywords VALUES(?, ?)', (keyword, ",".join(posts)))
 
 conn.commit()
 conn.close()
