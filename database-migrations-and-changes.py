3ca3c61bb36fbcd9d5a2c7b12b7900e9e8165c15
Rok <rok.ankaran@gmail.com>
2024-03-16 19:22:05 +0100

    encrypt user passwords

--- database-migrations-and-changes.py
+++ database-migrations-and-changes.py
@@ -1,43 +1,17 @@
 import sqlite3
+import encryption
 
 conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
 
-#Change keywors in database to lower case
-c.execute('SELECT * FROM posts')
-posts = c.fetchall()
+#Encrypt passwords
+c.execute("SELECT * FROM users")
+users = c.fetchall()
 
-for post in posts:
-    post_keywords = post[6].split(",")
-    post_keywords_lower = []
-    for keyword in post_keywords:
-        post_keywords_lower.append(keyword.lower())
-    c.execute('UPDATE posts SET keywords=? WHERE id =?', (",".join(post_keywords_lower), post[4]))
-
-#Create keywords table
-CREATE_KEYWORDS_TABLE = """
-CREATE TABLE IF NOT EXISTS keywords(
-keyword TEXT UNIQUE,
-posts TEXT
-)
-"""
-c.execute(CREATE_KEYWORDS_TABLE)
-
-c.execute('SELECT * FROM posts')
-posts = c.fetchall()
-
-keywords_dict = {}
-
-for post in posts:
-    post_keywords = post[6].split(",")
-    for keyword in post_keywords:
-        try:
-            keywords_dict[keyword].append(post[4])
-        except KeyError:
-            keywords_dict[keyword] = [post[4]]
-
-for keyword, posts in keywords_dict.items():
-    c.execute('INSERT INTO keywords VALUES(?, ?)', (keyword, ",".join(posts)))
+for user in users:
+    password = user[2]
+    id = user[1]
+    c.execute('UPDATE users SET password=? WHERE id=?', (encryption.encrypt(password), id))
 
 conn.commit()
 conn.close()
