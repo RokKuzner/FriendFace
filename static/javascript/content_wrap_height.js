d08a1eeebd1844cffb9a42474afc51b5c4c6ba57
Rok <rok.ankaran@gmail.com>
2024-03-24 18:20:23 +0100

    no navbar element error fix

--- static/javascript/content_wrap_height.js
+++ static/javascript/content_wrap_height.js
@@ -2,7 +2,13 @@ let content_wrap_element = document.querySelector(".content-wrap")
 let nav_element = document.querySelector(".navbar")
 
 function resize_content_wrap() {
-    let content_wrap_height = window.innerHeight - nav_element.offsetHeight
+    let content_wrap_height;
+    if (nav_element) {
+        content_wrap_height = window.innerHeight - nav_element.offsetHeight
+    } else {
+        content_wrap_height = window.innerHeight
+    }
+
     content_wrap_element.style.height = content_wrap_height + "px"
 }
 
