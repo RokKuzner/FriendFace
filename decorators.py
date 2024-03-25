b55cc339344540ef1d6c41dae5c7a34a05981618
Rok <rok.ankaran@gmail.com>
2024-03-25 19:00:55 +0100

    decorator that returns json error if user not logged in

--- decorators.py
+++ decorators.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect
+from django.http import JsonResponse
 
 def login_required(f):
     def decorated_function(request, *args, **kwargs):
@@ -12,3 +13,16 @@ def login_required(f):
         return f(request, *args, **kwargs)
 
     return decorated_function
+
+def login_required_json_response(f):
+    def decorated_function_for_login_required_json_response(request, *args, **kwargs):
+        if "current_user" in request.session:
+            if request.session["current_user"] is not None:
+                pass
+            else:
+                return JsonResponse({"status": "error", "message":"user not logged in"}, status=403)
+        else:
+             return JsonResponse({"status": "error", "message":"user not logged in"}, status=403)
+        return f(request, *args, **kwargs)
+
+    return decorated_function_for_login_required_json_response
