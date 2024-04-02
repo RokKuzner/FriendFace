7ca3495608836f088fb283679bdf4f33d4fad098
Rok <rok.ankaran@gmail.com>
2024-04-02 18:37:31 +0200

    function for calculating required textarea height

--- static/javascript/text_area_expend.js
+++ static/javascript/text_area_expend.js
@@ -22,3 +22,22 @@ function handle_textarea_input() {
 
     element.style.height = required_height + "px"
 }
+
+function get_text_height(text, container_width) { //this function calculates the height of text in a div element with specified style
+    let new_element = document.createElement("div")
+
+    new_element.style.width = container_width + "px"
+    new_element.style.overflowWrap = "break-word"
+    new_element.style.wordWrap = "break-word"
+    new_element.style.wordBreak = "break-word"
+    new_element.style.visibility = "hidden"
+
+    new_element.innerText = text
+    document.body.appendChild(new_element)
+
+    height = new_element.offsetHeight
+
+    document.body.removeChild(new_element)
+
+    return height
+}
