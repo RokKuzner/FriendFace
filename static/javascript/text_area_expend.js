e7926d5565b72284bdfb284ef96e07be177f849b
Rok <rok.ankaran@gmail.com>
2024-04-02 19:19:32 +0200

    resolved overflowY incorrect values

--- static/javascript/text_area_expend.js
+++ static/javascript/text_area_expend.js
@@ -14,12 +14,11 @@ function handle_textarea_input() {
     let max_height = Number(computed_style.maxHeight.slice(0, -2)) //slice(0, -2) returns only the number (ex. 200) instead of ex. 200px
 
     if (required_height > max_height) {
-        required_height = max_height + "px"
+        required_height = max_height
         element.style.overflowY = "scroll"
     } else {
-        element.style.overflowY = "none"
+        element.style.overflowY = "hidden"
     }
-
     element.style.height = required_height + "px"
 }
 
