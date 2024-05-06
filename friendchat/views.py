cf6101eeb0739f757e01ca8efc3350f5c9ccee98
Rok <rok.ankaran@gmail.com>
2024-05-06 20:14:13 +0200

    base HTML and render data for base.html

--- friendchat/views.py
+++ friendchat/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from decorators import login_required
+import database as db
 
-# Create your views here.
+@login_required
 def home(request):
-    return render(request, 'friendchatindex.html')
+    return render(request, 'friendchatindex.html', {'logged_in':True, 'current_user':request.session['current_user'],
+                                          'current_user_id': db.get_users_id_by_username(request.session['current_user']),
+                                          'this_url':str('/')})
