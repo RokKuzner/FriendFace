4959fb04c188d0f40e1191a8f16b871cddb091b6
Rok <rok.ankaran@gmail.com>
2024-05-06 18:16:35 +0200

    added friendface tables to database-create

--- database-create.py
+++ database-create.py
@@ -84,6 +84,24 @@ posts TEXT
 )
 """
 
+FRIENDCHAT_DMS = """
+CREATE TABLE IF NOT EXISTS chat_dms(
+user1 TEXT,
+user2 TEXT,
+dm_id TEXT UNIQUE
+)
+"""
+
+FRIENDCHAT_MESSAGES = """
+CREATE TABLE IF NOT EXISTS chat_msgs(
+dm_id TEXT,
+message_id TEXT UNIQUE,
+sender TEXT,
+time TEXT,
+content TEXT
+)
+"""
+
 c.execute(USERS)
 c.execute(POSTS)
 c.execute(COMMENTS)
@@ -93,6 +111,8 @@ c.execute(READ_POSTS)
 c.execute(DELETED_POSTS)
 c.execute(ADMIN_USERS)
 c.execute(KEYWORDS)
+c.execute(FRIENDCHAT_DMS)
+c.execute(FRIENDCHAT_MESSAGES)
 
 conn.commit()
 conn.close()
