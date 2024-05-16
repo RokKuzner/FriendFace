efc85c2810d626f6e946791bb789d92fdacfb95c
Rok <rok.ankaran@gmail.com>
2024-05-16 17:09:39 +0200

    removed the pront

--- friendchat/views.py
+++ friendchat/views.py
@@ -6,9 +6,6 @@ import database as db
 def home(request):
     current_user_id = db.get_users_id_by_username(request.session['current_user'])
 
-    print(db.get_all_users_dm_companions(current_user_id))
-
-
     return render(request, 'friendchatindex.html', {'logged_in':True, 'current_user':request.session['current_user'],
                                           'current_user_id': current_user_id,
                                           'this_url':str('/'),
