3d09c924cbdea384883b44bcec65a9e92e7b0909
Rok <rok.ankaran@gmail.com>
2024-05-16 17:08:40 +0200

    return dms to DTL

--- friendchat/views.py
+++ friendchat/views.py
@@ -4,6 +4,12 @@ import database as db
 
 @login_required
 def home(request):
+    current_user_id = db.get_users_id_by_username(request.session['current_user'])
+
+    print(db.get_all_users_dm_companions(current_user_id))
+
+
     return render(request, 'friendchatindex.html', {'logged_in':True, 'current_user':request.session['current_user'],
-                                          'current_user_id': db.get_users_id_by_username(request.session['current_user']),
-                                          'this_url':str('/')})
+                                          'current_user_id': current_user_id,
+                                          'this_url':str('/'),
+                                          "dms":db.get_all_users_dm_companions(current_user_id)})
