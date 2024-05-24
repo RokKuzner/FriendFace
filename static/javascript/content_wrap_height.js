b5aa0aa00306cf5cfd31450fcef42fa465f3ecfe
Rok <rok.ankaran@gmail.com>
2024-05-24 15:33:41 +0200

    better solution to subtracting multiple elements

--- static/javascript/content_wrap_height.js
+++ static/javascript/content_wrap_height.js
@@ -1,12 +1,16 @@
 let content_wrap_element = document.querySelector(".content-wrap")
-let nav_element = document.querySelector(".navbar")
+
+let element_classes_to_subtract = ["navbar", "top-corresponder-info"]
 
 function resize_content_wrap() {
-    let content_wrap_height;
-    if (nav_element) {
-        content_wrap_height = window.innerHeight - nav_element.offsetHeight
-    } else {
-        content_wrap_height = window.innerHeight
+    let content_wrap_height = window.innerHeight
+
+    for (let element_class_to_subtract of element_classes_to_subtract) {
+        let element_to_subtract = document.querySelector("." + element_class_to_subtract)
+
+        if (element_to_subtract) {
+            content_wrap_height -= element_to_subtract.offsetHeight
+        }
     }
 
     content_wrap_element.style.height = content_wrap_height + "px"
