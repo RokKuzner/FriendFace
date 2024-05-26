dbcccdd273f57ceed0492f0f1864d858caf8229e
Rok <rok.ankaran@gmail.com>
2024-05-26 16:56:18 +0200

    observe if elements resize

--- static/javascript/content_wrap_height.js
+++ static/javascript/content_wrap_height.js
@@ -2,12 +2,16 @@ let content_wrap_element = document.querySelector(".content-wrap")
 
 let element_classes_to_subtract = ["navbar", "top-corresponder-info"]
 
+let elements_to_subtract = []
+for (let element_class_to_subtract of element_classes_to_subtract) {
+    elements_to_subtract.push(document.querySelector("." + element_class_to_subtract))
+}
+
+
 function resize_content_wrap() {
     let content_wrap_height = window.innerHeight
 
-    for (let element_class_to_subtract of element_classes_to_subtract) {
-        let element_to_subtract = document.querySelector("." + element_class_to_subtract)
-
+    for (let element_to_subtract of elements_to_subtract) {
         if (element_to_subtract) {
             content_wrap_height -= element_to_subtract.offsetHeight
         }
@@ -18,3 +22,11 @@ function resize_content_wrap() {
 
 window.onload = resize_content_wrap
 window.onresize = resize_content_wrap
+
+let resize_observer = new ResizeObserver(entries => {
+    resize_content_wrap()
+})
+
+elements_to_subtract.forEach(element => {
+    resize_observer.observe(element)
+})
