e36cdbe59cd9dd15dc2a6fb51bec8fe657be9bbf
Rok <rok.ankaran@gmail.com>
2024-05-26 19:03:10 +0200

    update messages only on window load and if there is a new message

--- static/javascript/dm_messages_update.js
+++ static/javascript/dm_messages_update.js
@@ -1,3 +1,5 @@
+var last_messages = []
+
 async function get_messages() {
     let request_url = window.location.origin + "/api/getdmmessages/" + dm_id
     let response = await fetch(request_url)
@@ -8,31 +10,36 @@ async function get_messages() {
 
 async function display_messages() {
     let messages = await get_messages()
-    let content_wrap_element = document.querySelector(".content-wrap")
 
-    content_wrap_element.innerHTML = ""
-    for (let message of messages) {
+    if (last_messages.length == 0 || messages[messages.length - 1].message_id != last_messages[last_messages.length - 1].message_id) {
+        last_messages = messages
 
-        //create a new div element for the message
-        let message_div = document.createElement("div")
-        message_div.classList.add("message")
+        let content_wrap_element = document.querySelector(".content-wrap")
+        content_wrap_element.innerHTML = ""
 
-        if (message.sender_id == current_user_id) {
-            message_div.classList.add("message-mine")
-        } else {
-            message_div.classList.add("message-other")
+        for (let message of messages) {
+
+            //create a new div element for the message
+            let message_div = document.createElement("div")
+            message_div.classList.add("message")
+
+            if (message.sender_id == current_user_id) {
+                message_div.classList.add("message-mine")
+            } else {
+                message_div.classList.add("message-other")
+            }
+
+            //create a new div element for message content
+            let message_content_div = document.createElement("div")
+            message_content_div.classList.add("content")
+            message_content_div.innerText = message.content
+
+            //appent the message content div to the message div
+            message_div.appendChild(message_content_div)
+
+
+            content_wrap_element.appendChild(message_div)
         }
-
-        //create a new div element for message content
-        let message_content_div = document.createElement("div")
-        message_content_div.classList.add("content")
-        message_content_div.innerText = message.content
-
-        //appent the message content div to the message div
-        message_div.appendChild(message_content_div)
-
-
-        content_wrap_element.appendChild(message_div)
     }
 }
 
