c1d36accddfa8142ef35f47539458f5e2ab98a28
Rok <rok.ankaran@gmail.com>
2024-05-30 14:32:09 +0200

    new dm url

--- friendchat/urls.py
+++ friendchat/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('dm/<str:dm_id>', views.dm, name='dm')
+  path('dm/<str:dm_id>', views.dm, name='dm'),
+  path('new', views.new_dm, name='add new dm')
 ]
