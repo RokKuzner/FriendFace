325362b4bba9a47a92272f3b5fdb13984dcb0083
Rok <rok.ankaran@gmail.com>
2024-06-06 15:55:10 +0200

    fixed typo

--- FriendChatTyping.py
+++ FriendChatTyping.py
@@ -1,6 +1,6 @@
 from database import get_dm_members, get_utc_timestamp, get_username_by_user_id
 
-currenty_typing_by_dm = {}
+currently_typing_by_dm = {}
 
 """
 User typing object {"user_id":"923Esdvf8013aD6", "user_username":"a random username", "last_typing_time":2124134.39867}
@@ -13,22 +13,22 @@ def start_typing(dm_id:str, user_id:str):
 
     if is_user_typing_in_dm(dm_id, user_id) == False:
         try:
-            currenty_typing_by_dm[dm_id].append({"user_id":user_id, "user_username":get_username_by_user_id(user_id), "last_typing_time":float(get_utc_timestamp())})
+            currently_typing_by_dm[dm_id].append({"user_id":user_id, "user_username":get_username_by_user_id(user_id), "last_typing_time":float(get_utc_timestamp())})
         except KeyError:
-            currenty_typing_by_dm[dm_id] = [{"user_id":user_id, "user_username":get_username_by_user_id(user_id), "last_typing_time":float(get_utc_timestamp())}]
+            currently_typing_by_dm[dm_id] = [{"user_id":user_id, "user_username":get_username_by_user_id(user_id), "last_typing_time":float(get_utc_timestamp())}]
     else:
         get_user_typing_object_in_dm(dm_id, user_id)["last_typing_time"] = float(get_utc_timestamp())
 
 def stop_typing(dm_id:str, user_id:str):
 
     if is_user_typing_in_dm(dm_id, user_id):
-        currenty_typing_by_dm[dm_id].remove(get_user_typing_object_in_dm(dm_id, user_id))
+        currently_typing_by_dm[dm_id].remove(get_user_typing_object_in_dm(dm_id, user_id))
 
 def get_currently_typing_in_dm(dm_id:str):
     try:
         remove_users_not_typing(dm_id)
 
-        return currenty_typing_by_dm[dm_id]
+        return currently_typing_by_dm[dm_id]
     except KeyError:
         return []
 
@@ -37,7 +37,7 @@ def is_user_typing_in_dm(dm_id:str, user_id:str):
     
 def get_user_typing_object_in_dm(dm_id:str, user_id:str):
     try:
-        currently_typing_in_this_dm = currenty_typing_by_dm[dm_id]
+        currently_typing_in_this_dm = currently_typing_by_dm[dm_id]
 
         for user_typing in currently_typing_in_this_dm:
             if user_typing["user_id"] == user_id:
@@ -49,7 +49,7 @@ def get_user_typing_object_in_dm(dm_id:str, user_id:str):
 
 def remove_users_not_typing(dm_id:str, not_typing_treshold=3):
     try:
-        users_typing = currenty_typing_by_dm[dm_id]
+        users_typing = currently_typing_by_dm[dm_id]
     except KeyError:
         users_typing = []
 
