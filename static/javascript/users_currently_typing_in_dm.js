8dc972e456485d9a3894ccf11486941915dd093d
Rok <rok.ankaran@gmail.com>
2024-06-08 21:47:55 +0200

    users currently typing in dm javascript code changed for websockets

--- static/javascript/users_currently_typing_in_dm.js
+++ static/javascript/users_currently_typing_in_dm.js
@@ -1,29 +1,7 @@
 let send_message_text_area = document.querySelector(".send-message-textarea")
 let typing_status_element = document.querySelector(".top-corresponder-info-username .typing-status")
 
-async function get_users_typing() {
-    let request_url = window.location.origin + "/api/getcurrentlytyping?dm_id=" + dm_id
-    let response = await fetch(request_url)
-    let json_response = await response.json()
-
-    return json_response["users typing"]
-}
-
-async function set_user_typing_status(typing_status) {
-    let request_url = window.location.origin + "/api/settypingstatus?dm_id=" + dm_id + "&typing=" + typing_status
-    let response = await fetch(request_url)
-}
-
-send_message_text_area.addEventListener("input", () => {
-    if (send_message_text_area.value == "") {
-        set_user_typing_status("false")
-    } else {
-        set_user_typing_status("true")
-    }
-})
-
-async function update_typing_status() {
-    let users_typing = await get_users_typing()
+function update_typing_status(users_typing) {
     let corresponder_is_typing = false
 
     for (let user_typing of users_typing) {
@@ -40,4 +18,24 @@ async function update_typing_status() {
     }
 }
 
-setInterval(update_typing_status, 500)
+const dm_typing_socket_url = "ws://" + window.location.host + "/ws/dm-typing/" + dm_id
+const dm_typing_socket = new WebSocket(dm_typing_socket_url)
+
+dm_typing_socket.onmessage = function(e) {
+    let data = JSON.parse(e.data)
+    update_typing_status(data["users typing"])
+}
+
+send_message_text_area.addEventListener("input", () => {
+    let is_typing;
+
+    if (send_message_text_area.value == "") {
+        is_typing = "false"
+    } else {
+        is_typing = "true"
+    }
+
+    dm_typing_socket.send(JSON.stringify({
+        "is_typing": is_typing
+    }))
+})
