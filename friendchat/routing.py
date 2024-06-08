eec07d172d08ee83b130aa47aa3e17a151d32336
Rok <rok.ankaran@gmail.com>
2024-06-08 21:43:51 +0200

    DmTyping consumer class and route

--- friendchat/routing.py
+++ friendchat/routing.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import consumers
 
 websochet_urlpatterns = [
-  path("ws/dm-messages/<str:dm_id>", consumers.DmMessagesConsumer.as_asgi())
+  path("ws/dm-messages/<str:dm_id>", consumers.DmMessagesConsumer.as_asgi()),
+  path("ws/dm-typing/<str:dm_id>", consumers.DmTyping.as_asgi())
 ]
