df42e69fc0e18d1823b506de0bf29ab1bb3bfb68
Rok <rok.ankaran@gmail.com>
2024-06-08 17:20:24 +0200

    channel layers

--- FriendFace/settings.py
+++ FriendFace/settings.py
@@ -50,6 +50,12 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = "FriendFace.asgi.application"
 
+CHANNEL_LAYERS = {
+  "default": {
+    "BACKEND": "channels.layers.InMemoryChannelLayer"
+  }
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
