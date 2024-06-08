d295077f74e0334c75306b5cc529d4577ba64126
Rok <rok.ankaran@gmail.com>
2024-06-08 13:57:11 +0200

    set up asgi application

--- FriendFace/settings.py
+++ FriendFace/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     "servemedia"
 ]
 
+ASGI_APPLICATION = "FriendFace.asgi.application"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
