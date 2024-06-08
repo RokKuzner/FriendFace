81814fa05e0ce67fe70dcf5d83e3f7ec8704fced
Rok <rok.ankaran@gmail.com>
2024-06-08 17:18:04 +0200

    DmMessagesConsumer recive function

--- friendchat/consumers.py
+++ friendchat/consumers.py
@@ -17,3 +17,18 @@ class DmMessagesConsumer(AsyncWebsocketConsumer):
       "staus": "connected",
       "messages": db.get_dm_messages(dm_id)
     }))
+
+  async def receive(self, text_data):
+    dm_id = self.scope['url_route']['kwargs']['dm_id']
+    current_user_username = self.scope["session"]["current_user"]
+    current_user_id = db.get_users_id_by_username(current_user_username)
+  
+    text_data_json = json.loads(text_data)
+    message_content = text_data_json["message"]
+
+    db.new_message(dm_id, current_user_id, message_content)
+
+    await self.send(text_data=json.dumps({
+      "staus": "connected",
+      "messages": db.get_dm_messages(dm_id)
+    }))
