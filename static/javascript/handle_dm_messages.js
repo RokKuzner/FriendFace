f9d96930a1814229c2bda1379dd2542b353c38cf
Rok <rok.ankaran@gmail.com>
2024-06-08 17:48:25 +0200

    removed unnecesary if and latest_messages

--- static/javascript/handle_dm_messages.js
+++ static/javascript/handle_dm_messages.js
@@ -1,45 +1,40 @@
 let send_message_form = document.querySelector(".send-message-form")
-var last_messages = []
 
 function display_messages(messages) {
-    if (last_messages.length == 0 || messages[messages.length - 1].message_id != last_messages[last_messages.length - 1].message_id) {
-        last_messages = messages
+    let content_wrap_element = document.querySelector(".content-wrap")
+    content_wrap_element.innerHTML = ""
 
-        let content_wrap_element = document.querySelector(".content-wrap")
-        content_wrap_element.innerHTML = ""
+    for (let message of messages) {
 
-        for (let message of messages) {
+        //create a new div element for the message
+        let message_div = document.createElement("div")
+        message_div.id = message.message_id
+        message_div.classList.add("message")
 
-            //create a new div element for the message
-            let message_div = document.createElement("div")
-            message_div.id = message.message_id
-            message_div.classList.add("message")
-
-            if (message.sender_id == current_user_id) {
-                message_div.classList.add("message-mine")
-            } else {
-                message_div.classList.add("message-other")
-            }
-
-            //create a new div element for message time
-            let message_time_div = document.createElement("div")
-            message_time_div.classList.add("time")
-            message_time_div.innerText = message.time_pretty
-            message_div.appendChild(message_time_div)
-
-            //create a new div element for message content
-            let message_content_div = document.createElement("div")
-            message_content_div.classList.add("content")
-            message_content_div.innerText = message.content
-            message_div.appendChild(message_content_div)
-            
-            content_wrap_element.appendChild(message_div)
+        if (message.sender_id == current_user_id) {
+            message_div.classList.add("message-mine")
+        } else {
+            message_div.classList.add("message-other")
         }
+
+        //create a new div element for message time
+        let message_time_div = document.createElement("div")
+        message_time_div.classList.add("time")
+        message_time_div.innerText = message.time_pretty
+        message_div.appendChild(message_time_div)
+
+        //create a new div element for message content
+        let message_content_div = document.createElement("div")
+        message_content_div.classList.add("content")
+        message_content_div.innerText = message.content
+        message_div.appendChild(message_content_div)
         
-        //Scroll to bottom
-        content_wrap_element = document.querySelector(".content-wrap")
-        content_wrap_element.scrollTop = content_wrap_element.scrollHeight
+        content_wrap_element.appendChild(message_div)
     }
+    
+    //Scroll to bottom
+    content_wrap_element = document.querySelector(".content-wrap")
+    content_wrap_element.scrollTop = content_wrap_element.scrollHeight
 }
 
 const dm_messages_socket_url = "ws://" + window.location.host + "/ws/dm-messages/" + dm_id
