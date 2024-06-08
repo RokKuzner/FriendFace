c1b4648bf859a4ecd04c27b98ab27a292531cf71
Rok <rok.ankaran@gmail.com>
2024-06-08 17:32:14 +0200

    removed api functions not in use

--- api/urls.py
+++ api/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
   path('userexists', views.userexists, name='userexists'),
   path('post', views.post, name='post'),
   path('deletepost', views.delete_post, name='deletepost'),
-  path('newdmmessage', views.new_dm_message, name='deletepost'),
-  path('getdmmessages/<str:dm_id>', views.get_dm_messages, name='get dm messages'),
   path('settypingstatus', views.set_currently_typing_in_dm, name='set user typing status in friendchat dm'),
   path('getcurrentlytyping', views.get_currently_typing_in_dm, name='get users currently typing friendchat dm')
 ]
