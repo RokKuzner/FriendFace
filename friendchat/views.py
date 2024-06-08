dfb3bafb0f0ebfe4c892df2570293caff1ea9c53
Rok <rok.ankaran@gmail.com>
2024-06-08 13:28:47 +0200

    separate folder for friendchat templates

--- friendchat/views.py
+++ friendchat/views.py
@@ -6,7 +6,7 @@ import database as db
 def home(request):
     current_user_id = db.get_users_id_by_username(request.session["current_user"])
 
-    return render(request, "friendchatindex.html", {"logged_in":True, "current_user":request.session["current_user"],
+    return render(request, "friendchat/friendchatindex.html", {"logged_in":True, "current_user":request.session["current_user"],
                                           "current_user_id": current_user_id,
                                           "this_url":str("/"),
                                           "dms":[db.get_dm_preview(dm[2], current_user_id) for dm in db.get_latest_active_dms_by_user(current_user_id)]})
@@ -21,7 +21,7 @@ def dm(request, dm_id):
     corresponder_id = db.get_dm_corresponder(dm_id, current_user_id)
     corresponder_username = db.get_username_by_user_id(corresponder_id)
     
-    return render(request, "dm.html", {"logged_in":True, "current_user":request.session["current_user"],
+    return render(request, "friendchat/dm.html", {"logged_in":True, "current_user":request.session["current_user"],
                                           "current_user_id": current_user_id,
                                           "this_url":str("/"),
                                           "corresponder_username": corresponder_username,
@@ -34,7 +34,7 @@ def new_dm(request):
     current_user_id = db.get_users_id_by_username(request.session["current_user"])
 
     if request.method == 'GET':
-        return render(request, "new_dm.html", {"logged_in":True, "current_user":request.session["current_user"],
+        return render(request, "friendchat/new_dm.html", {"logged_in":True, "current_user":request.session["current_user"],
                                           "current_user_id": current_user_id,
                                           "this_url":str("/")})
     
