39403c8c166df9e07f44aea73c98a245394646f4
Rok <rok.ankaran@gmail.com>
2024-06-08 22:30:35 +0200

    set_typing_status

--- static/javascript/users_currently_typing_in_dm.js
+++ static/javascript/users_currently_typing_in_dm.js
@@ -23,6 +23,21 @@ function update_typing_status(users_typing) {
 const dm_typing_socket_url = "ws://" + window.location.host + "/ws/dm-typing/" + dm_id
 const dm_typing_socket = new WebSocket(dm_typing_socket_url)
 
+function set_typing_status(status) {
+    if (status != "true" && status != "false") {
+        return "status can be a string true or false"
+    }
+
+    //save the data
+    last_typing_data["time"] = Date.now()
+    last_typing_data["is_typing"] = status
+
+    //send typing status to server
+    dm_typing_socket.send(JSON.stringify({
+        "is_typing": status
+    }))
+}
+
 dm_typing_socket.onmessage = function(e) {
     let data = JSON.parse(e.data)
     update_typing_status(data["users typing"])
@@ -37,25 +52,11 @@ send_message_text_area.addEventListener("input", () => {
         is_typing = "true"
     }
 
-    //save the data
-    last_typing_data["time"] = Date.now()
-    last_typing_data["is_typing"] = is_typing
-
-    //send typing status to server
-    dm_typing_socket.send(JSON.stringify({
-        "is_typing": is_typing
-    }))
+    set_typing_status(is_typing)
 })
 
 setInterval(()=>{
     if ( Date.now() - last_typing_data["time"] > 3000 && last_typing_data["is_typing"] == "true") {
-        //save the data
-        last_typing_data["time"] = Date.now()
-        last_typing_data["is_typing"] = "false"
-
-        //send typing status to server
-        dm_typing_socket.send(JSON.stringify({
-            "is_typing": "false"
-        }))
+        set_typing_status("false")
     }
 }, 500)
